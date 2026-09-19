7a7c8a273bdee23ac2378b93bae894cc4c3ce223
Etornam <navidben45@gmail.com>
2026-09-19 20:08:06 +0000

    Chore: Update example with new API

--- examples/basic/schema/product.go
+++ examples/basic/schema/product.go
@@ -2,15 +2,11 @@ package schema
 
 import "github.com/etornam45/grain/schema"
 
-type ProductColums struct {
-	Id, Name, Price, Sku, Barcode *schema.ColumnDef
-}
-
-var Product = schema.Table[ProductColums]("product",
+var Product = schema.Table("product",
 	schema.Column("id", schema.UUID()).PrimaryKey().NotNull(),
 	schema.Column("name", schema.Varchar(225)).NotNull().Index(),
 	schema.Column("price", schema.Numeric(10, 2)).NotNull(),
 	schema.Column("sku", schema.Varchar(15)).NotNull().Unique(),
 	schema.Column("barcode", schema.Int()).Index(),
 ).
-Index("idx_product_name_sku", "name", "sku")
+	Index("idx_product_name_sku", "name", "sku")
